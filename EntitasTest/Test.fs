d1aaa5322bfaffa5cef9613f993c0c31e6d9eb33
darkoverlordofdata <darkoverlordofdata@gmail.com>
2015-12-16 13:01:38 -0800

    init

---
+++ EntitasTest/Test.fs
@@ -0,0 +1,18 @@
+﻿module EntitasTest
+
+open System    
+open NUnit.Framework
+open Entitas
+
+let world = new World(16)
+
+[<TestFixture>]
+type FirstTest() =
+
+    [<Test>]
+    member this.TestIs1() =
+
+        let e = world.CreateEntity()
+        let id = e.Id
+        Assert.AreEqual(id, 1)
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+* ecs-fsharp
+
+experiments using an ecs in unity+fsharp

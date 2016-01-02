f7d7a0a8092af6248a7e6f7d00b741023de64b8d
darkoverlordofdata <darkoverlordofdata@gmail.com>
2016-01-01 23:32:53 -0800

    daily

--- ShmupWarz/Controllers/LeaderboardController.fs
+++ ShmupWarz/Controllers/LeaderboardController.fs
@@ -28,22 +28,17 @@ type LeaderboardController () =
 
 
         for r=0 to MAX-1 do
-            let mutable score = ""
-            let mutable yyyymmdd = ""
-            let mutable date = ""
-
-            if r<data.Count then
-                let row = JSON.Object(data.[r])
-                score <- (row.["score"]).ToString()
-                date <- (row.["date"]).ToString()
-                yyyymmdd <- (sprintf "%s/%s/%s" (date.[4..5]) (date.[6..7]) (date.[2..3]) )
-                //score <- Convert.ToString(row.["score"])
-                //yyyymmdd <- Convert.ToString(row.["date"])
-
             let col1 = GameObject.Find("UI Root (3D)/Camera/Anchor/Panel - Main/Window/Date"+r.ToString())
             let text1 = col1.GetComponent("UILabel"):?>UILabel
-            text1.text <- yyyymmdd
 
             let col2 = GameObject.Find("UI Root (3D)/Camera/Anchor/Panel - Main/Window/Score"+r.ToString())
             let text2 = col2.GetComponent("UILabel"):?>UILabel
-            text2.text <- score
+
+            if r<data.Count then
+                let row = JSON.Object(data.[r])
+                let date = (row.["date"]).ToString()
+                text1.text <- (sprintf "%s/%s/%s" (date.[4..5]) (date.[6..7]) (date.[2..3]) )
+                text2.text <- (row.["score"]).ToString()
+            else
+                text1.text <- ""
+                text2.text <- ""
